9ea9c1d00f12b88d36445b0bd6110914898af1f5
samitbarai <samit.barai@juspay.in>
2018-08-23 23:36:16 +0530

    gatsby boilerplate added

---
+++ website/gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'My Portfolio',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ website/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Samit Barai</h1>
+    <p>Welcome to my portfolio website.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
